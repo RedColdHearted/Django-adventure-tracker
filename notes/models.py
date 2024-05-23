627891ab14b7ce0633a117f9c84720fb0b69c31e
максим <maximovichpro@gmail.com>
2024-05-23 22:35:10 +0500

    note model new user_id field

--- notes/models.py
+++ notes/models.py
@@ -3,12 +3,15 @@ import uuid
 
 from django.db import models
 
+from users.models import User
+
+
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateField(default=datetime.date.today)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.title}"
 
